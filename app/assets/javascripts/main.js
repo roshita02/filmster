52c66f0860a403f23cc21616cb66640ddb354fb0
Roshita Shakya <roshi.shk@gmail.com>
2017-10-12 13:13:00 +0000

    Displaying movies based on search query

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,17 +7,26 @@ $(function(){
     displayMovies(data);
   });
 
- function displayMovies(data){
+function displayMovies(data){
+  let container = $("#movies");
   let htmlString = "";
+
+  container.empty();
+
   let imageUrl = getBaseImageUrl();
 
-  data["results"].forEach(function(movie){
-    htmlString += `<img src= ${imageUrl}/${movie["poster_path"]} />
-                   <p>${movie["title"]}</p>
-                   <p>${movie["overview"]}</p>`;
-  });
+  if (data["results"].length == 0) {
+    htmlString = `<div class="alert alert-danger text-center" role="alert">No Data Found!</div>`;
+  }
+  else{
+    data["results"].forEach(function(movie){
+      htmlString += `<img src=${movie["poster_path"] == null ? "/assets/default2.png" : imageUrl + "/" + movie["poster_path"]} />
+                     <p>${movie["title"]}</p>
+                     <p>${movie["overview"]}</p>`;
+    });
+  }
 
-  $("#movies").append(htmlString);
+  container.append(htmlString);
 }
   function getBaseImageUrl(){
   var url = "";
@@ -35,5 +44,16 @@ $(function(){
   });
   return url;
 }
-  
+  let form = $('#movie-search');
+form.submit(function(e){
+  e.preventDefault();
+
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/search/movie?api_key=a43d79c83f0d6a57f61f35174619d879',
+    data: form.serialize()
+  })
+  .done(function(data){
+    displayMovies(data);
+  });
+});
 });
